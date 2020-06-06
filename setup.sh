3da01b905ca93cced0ea439dda30fc727a59e808
Cyber Demon <XCobalt88@protonmail.com>
2020-06-06 18:52:00 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/usr/bin/bash
 sys=$(lsb_release -si)
 case $sys in
   ArchLinux|ManjaroLinux)
-    pamac install netcat --no-confirm
+    sys='pamac'
+    conf='--no-confirm'
     ;;
   *)
-    apt-get install netcat --no-confirm
+    sys='apt-get'
+    conf='-y'
     ;;
 esac
+clear
+"$sys" update "$conf" && "$sys" upgrade "$conf"
+"$sys" install netcat "$conf"
+
